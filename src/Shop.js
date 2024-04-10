275d4d5ab122fa2d5575511b4dd42a5e9f0ebbc2
racheldesa <racheldesa@outlook.com>
2024-04-10 09:05:06 -0500

    Cart functionality implemented in Shop.js. TODO: move cart to separate page.

--- src/Shop.js
+++ src/Shop.js
@@ -7,20 +7,22 @@ const Shop = () => {
     const [cartTotal, setCartTotal] = useState(0);
 
     const listItems = items.map((el) => (
-        <div key={el.id}>
-            <img class="img-fluid" src={el.image} width={150} /> <br/>
-
-            {el.title} <br/>
-            {el.category} <br/>
-            {el.price} <br/>
-
-            <div class="btn-group border rounded">
-                <span class="border">
-                <button type="button" class="btn btn-default" onClick={() => removeFromCart(el)}> - </button>{" "}
-                </span>
-                <span class="border">
-                <button type="button" class="btn btn-default" variant="light" onClick={() => addToCart(el)}> + </button>
-                </span>
+        <div class="row border-top border-bottom" key={el.id}>
+            <div class="row main align-items-center">
+                <div class="col-2">
+                <img class="img-fluid" src={el.image} />
+                </div>
+            <div class="col">
+                <div class="row text-muted">{el.title}</div>
+                <div class="row">{el.category}</div>
+            </div>
+            <div class="col">
+                <button type="button" variant="light" onClick={() => removeFromCart(el)}> - </button>{" "}
+                <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+            </div>
+            <div class="col">
+                ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
+            </div>
             </div>
         </div>
     ));
@@ -42,6 +44,11 @@ const Shop = () => {
         hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
         setCart(hardCopy);
     };
+
+    function howManyofThis(id) {
+        let hmot = cart.filter((cartItem) => cartItem.id === id);
+        return hmot.length;
+    }
 
     return (
         <div>
