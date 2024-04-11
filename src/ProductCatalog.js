3939d4744cf4102fd4710aa0a57e5c8051a14326
lukeped <lukeped@iastate.edu>
2024-04-11 04:51:26 -0500

    Prettifying done

--- src/ProductCatalog.js
+++ src/ProductCatalog.js
@@ -16,21 +16,21 @@ function ProductCatalog() {
     const Shop = () => {
         const listItems = ProductsCategory.map((el) => (
             <div class="col mb-4">
-                <div class="card h-100">
+                <div class="card h-100 bg-secondary-subtle shadow">
                     <div key={el.id}>
-                        <img class="card-img-top" src={el.image} />
+                        <img class="card-img-top p-4" src={el.image} />
                         <div class="card-body flex-column">
                             <h2 class="card-title">{el.title}</h2>
                             <div class="card-text"> 
-                                <p>{el.category} </p>
-                                <p>${el.price}</p> 
+                                <p class="text-primary">{el.category} </p>
+                                <p class="text-info-emphasis">${el.price}</p> 
                             </div>
                             <div class="btn-group border rounded">
                                 <span class="border">
-                                <button type="button" class="btn btn-default" onClick={() => removeFromCart(el)}> - </button>{" "}
+                                <button type="button" class="btn btn-default bg-light-subtle shadow border-dark-subtle" onClick={() => removeFromCart(el)}> - </button>
                                 </span>
                                 <span class="border">
-                                <button type="button" class="btn btn-default" variant="light" onClick={() => addToCart(el)}> + </button>
+                                <button type="button" class="btn btn-default bg-light-subtle shadow border-dark-subtle" variant="light" onClick={() => addToCart(el)}> + </button>
                                 </span>
                             </div>
                         </div>
@@ -107,7 +107,7 @@ function ProductCatalog() {
     // Function for Cart View
     const Cart = () => {
         const cartItems = cart.map((el) => (
-            <div class="row mb-4">
+            <div class="row mb-4 bg-secondary-subtle p-2 border border-dark-subtle shadow">
                 <div class="col">{el.title}</div>
                 <div class="col">{el.description}</div>
                 <div class="col">{el.count}</div>
@@ -128,18 +128,18 @@ function ProductCatalog() {
         }
         return (
             <div>
-                <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="container mt-4">
                     <div class="container mb-4">
                         <div class="row">
                             <div class="col">
                                 <div  className="form-group">
-                                    <input {...register("fullName", {required: true})} type="text" placeholder="Full Name" className="form-control" />
+                                    <input {...register("fullName", {required: true})} type="text" placeholder="Full Name" className="form-control border border-primary-subtle" />
                                     {errors.fullName && <p className="text-danger">Full Name is required.</p>}
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="col" className="form-group">
-                                    <input {...register("email", {required: true, pattern: /^\S+@\S+$/i})} placeholder="Email" className="form-control" />
+                                    <input {...register("email", {required: true, pattern: /^\S+@\S+$/i})} placeholder="Email" className="form-control border border-primary-subtle" />
                                     {errors.email && <p className="text-danger">Email is required.</p>}
                                 </div>
                             </div>
@@ -147,7 +147,7 @@ function ProductCatalog() {
                     </div>
                     <div class="container">
                         <div className="form-group" class="mb-4">
-                            <input {...register("creditCard", {required: true})} type="number" placeholder="Credit Card" className="form-control" />
+                            <input {...register("creditCard", {required: true})} type="number" placeholder="Credit Card" className="form-control border border-primary-subtle" />
                             {errors.creditCard && <p className="text-danger">Credit Card is required.</p>}
                         </div>
                     </div>
@@ -155,13 +155,13 @@ function ProductCatalog() {
                         <div class="row">
                             <div class="col">
                                 <div className="form-group">
-                                    <input {...register("address", {required: true, pattern:"\d{5,5}(-\d{4,4})?"})} placeholder="Address" className="form-control" />
+                                    <input {...register("address", {required: true, pattern:"\d{5,5}(-\d{4,4})?"})} placeholder="Address" className="form-control border border-primary-subtle" />
                                     {errors.address && <p className="text-danger">Address is required.</p>}
                                 </div>
                             </div>
                             <div class="col">
                                 <div className="form-group">
-                                    <input {...register("address2")} placeholder="Address 2" className="form-control" />
+                                    <input {...register("address2")} placeholder="Address 2" className="form-control border border-primary-subtle" />
                                 </div>
                             </div>
                         </div>
@@ -170,19 +170,19 @@ function ProductCatalog() {
                         <div class="row">
                             <div class="col">
                                 <div className="form-group">
-                                    <input {...register("city", {required: true})} placeholder="City" className="form-control"/>
+                                    <input {...register("city", {required: true})} placeholder="City" className="form-control border border-primary-subtle"/>
                                     {errors.city && <p className="text-danger">City is required.</p>}
                                 </div>
                             </div>
                             <div class="col">
                                 <div className="form-group">
-                                    <input {...register("state", {required: true})} placeholder="State" className="form-control"/>
+                                    <input {...register("state", {required: true})} placeholder="State" className="form-control border border-primary-subtle"/>
                                     {errors.state && <p className="text-danger">State is required.</p>}
                                 </div>
                             </div>
                             <div class="col">
                                 <div className="form-group">
-                                    <input {...register("zip", {required: true, pattern:"/^[0-9\b]+$/"})} type="number" placeholder="Zip"className="form-control" />
+                                    <input {...register("zip", {required: true, pattern:"/^[0-9\b]+$/"})} type="number" placeholder="Zip"className="form-control border border-primary-subtle" />
                                     {errors.zip && <p className="text-danger">Zip is required.</p>}
                                 </div>
                             </div>
@@ -200,10 +200,14 @@ function ProductCatalog() {
     function confirm() {
         setView(2);
     }
-    function backToBrowse() {
+    function backToBrowseFresh() {
         setCart([]);
         setCartTotal(0);
         setDataF({});
+        setView(0);
+    }
+
+    function backToBrowse() {
         setView(0);
     }
 
@@ -211,13 +215,14 @@ function ProductCatalog() {
         return (
             <div>
                 <span class="border border-white">
-                    <div class="btn-group mb-4">
+                    <div class="btn-group mb-4 shadow">
                         <button type="button" class="btn btn-primary border" onClick={backToBrowse}>Shop</button>
                         <button type="button" class="btn btn-primary border" onClick={goToCart}>Cart</button>
                     </div>
                 </span>
-                <div className="py-10">
-                    <input class="form-control mr-sm-2 mb-4" type="search" placeholder="Search" aria-label="Search" value={query} onChange={handleSearch} />
+                <div className="">
+                    <h1 class="mb-3 fs-2 fw-bold ">Let's Shop!</h1>
+                    <input class="form-control mr-sm-2 mb-4 shadow" type="search" placeholder="Search" aria-label="Search" value={query} onChange={handleSearch} />
                     <Shop />
                 </div>
                 <button type="button" class="btn btn-primary" onClick={goToCart}>View Cart</button>
@@ -230,32 +235,32 @@ function ProductCatalog() {
             <div class="mt-4">
                 <div>
                     <span class="border border-white mb-4">
-                        <div class="btn-group mb-4">
+                        <div class="btn-group mb-4 shadow">
                             <button type="button" class="btn btn-primary border" onClick={backToBrowse}>Shop</button>
                             <button type="button" class="btn btn-primary border" onClick={goToCart}>Cart</button>
                         </div>
                     </span>
                 </div>
-                <button type="button" class="btn btn-primary mb-4" onClick={backToBrowse}>Return</button>
-                <h2>My Cart</h2>
+                <button type="button" class="btn btn-primary" onClick={backToBrowse}>Return</button>
+                <h2 class="fs-1 text-center mb-4 text-decoration-underline fw-semibold">My Cart</h2>
                 <div class="container">
                     <div class="row mb-4">
-                        <div class="col">Name</div>
-                        <div class="col">Description</div>
-                        <div class="col">Count</div>
-                        <div class="col">Price</div>
-                        <div class="col"></div>
+                        <div class="col text-decoration-underline">Name</div>
+                        <div class="col text-decoration-underline">Description</div>
+                        <div class="col text-decoration-underline">Count</div>
+                        <div class="col text-decoration-underline">Price</div>
+                        <div class="col text-decoration-underline">Photo</div>
                     </div>
                     <Cart />
-                    <div class="row mb-4">
+                    <div class="row mb-4 border border-secondary-subtle">
                         <div class="col"></div>
                         <div class="col"></div>
-                        <div class="col">Total</div>
-                        <div class="col">${cartTotal}</div>
+                        <div class="col fw-semibold">Total</div>
+                        <div class="col fw-semibold">${cartTotal}</div>
                         <div class="col"></div>
                     </div>
                 </div>
-                <h1>Payment Information</h1>
+                <h1 class="fs-2 text-center text-decoration-underline fw-semibold">Payment Information</h1>
                 <Payment />
             </div>
         );
@@ -265,8 +270,11 @@ function ProductCatalog() {
     const Confirmation = () => {
         const confirmedItems = cart.map((el) => (
             <div class="col mb-4">
-                <h1 class="m-3 text-center">{el.title}</h1>
-                <img class="card-img-top m-4 border" src={el.image} />
+                <div class="card border bg-secondary-subtle shadow-sm">
+                    <h1 class="m-3 text-center">{el.title}</h1>
+                    <img class="card-img-top p-4" src={el.image} />
+                </div>
+                
             </div>
         ));
         const maskCreditCard = ((creditCardIDInput) => {
@@ -281,17 +289,16 @@ function ProductCatalog() {
             return creditCardID;
         });
         return (
-            <div>
-                <h1 class="m-3">Order Confirmed!</h1>
-                <h1 class="m-3">Order items:</h1>
+            <div class="p-3">
+                <h1 class="mb-4 fw-bold fs-1">Order Confirmed!</h1>
                 <div class="container">
-                    <div class="row row-cols-4">
+                    <div class="row row-cols-3">
                         {confirmedItems}
                     </div>
                 </div>
-                <div class="m-3">Order total: {cartTotal}</div>
-                <div class="m-3">
-                    <h1>Order details: </h1>
+                <div class="fs-3 fw-semibold mb-2">Order total: {cartTotal}</div>
+                <div class="">
+                    <h1 class="fs-5">Order details: </h1>
                     <p>Name: {dataF.fullName}</p>
                     <p>Email: {dataF.email}</p>
                     <p>Ordered with Credit Card: {maskCreditCard(dataF.creditCard)}</p>
@@ -304,7 +311,7 @@ function ProductCatalog() {
         return (
             <div>
                 <Confirmation/>
-                <button type="button" class="btn btn-primary mt-4" onClick={backToBrowse}>Back to Browse</button>
+                <button type="button" class="btn btn-primary mt-4" onClick={backToBrowseFresh}>Back to Browse</button>
             </div>
         );
     }
@@ -323,7 +330,7 @@ function ProductCatalog() {
     
 
     return (
-        <div class="m-4">
+        <div class="p-4 bg-body-tertiary">
             {view === 0 && <ShopView />}
             {view === 1 && <CartView />}
             {view === 2 && <ConfirmationView />}
